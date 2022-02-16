e1da6f38da4efb8533ef3ccbb53821926364cf59
shawnshivdat <shawnshivdat@gmail.com>
2022-02-15 23:05:43 -0500

    session 1

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct MeetingView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -17,29 +17,38 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack{
+
+                ProgressView(value: 5, total: 15)
+                HStack {
+                    VStack(alignment: .leading){
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing){
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.tophalf.fill")
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .accessibilityElement(children:.ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack{
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action:{}) {
+                        Image(systemName:"forward.fill")
                     }
+                    .accessibilityLabel("Next speaker")
+                    
                 }
+                .padding()
             }
-            Text("Select an item")
-        }
     }
 
     private func addItem() {
@@ -81,8 +90,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        MeetingView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -13,7 +13,7 @@ struct ScrumdingerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
